de11c4f98da7a016f69c23c829b0ab5c02ddf7d1
Mikołaj Koszczyc <mikoszczyc@gmail.com>
2021-11-09 00:20:16 +0100

    Loading, button A and button B

--- 08_11_2021/08_11_2021/MainWindow.xaml.cs
+++ 08_11_2021/08_11_2021/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.IO;
 using System.Drawing;
+using System.Diagnostics;
 
 namespace _08_11_2021
 {
@@ -23,15 +24,12 @@ namespace _08_11_2021
     /// </summary>
     public partial class MainWindow : Window
     {
+        BitmapImage obrazek;
+        int FlipX = 1;
         public MainWindow()
         {
             InitializeComponent();
         }
-        BitmapImage obrazek;
-        Bitmap obrazekBmp;
-
-        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
-        public static extern bool DeleteObject(IntPtr hObject);
 
         private void Button_Click_Choose(object sender, RoutedEventArgs e)
         {
@@ -41,30 +39,59 @@ namespace _08_11_2021
                 Uri fileUri = new Uri(openFileDialog.FileName);
                 obrazek = new BitmapImage(fileUri);
                 ImgViewer.Source = obrazek;
-                obrazekBmp = new Bitmap(obrazek.StreamSource);
-                //BitmapImage
             }
-            
         }
 
         private void Button_Click_A(object sender, RoutedEventArgs e)
         {
             // mirror
-            obrazekBmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            if (obrazek != null)
+            {
+                ScaleTransform flipTransform = new ScaleTransform();
+                FlipX *= -1;
+                flipTransform.ScaleX = FlipX;
+                ImgViewer.RenderTransform = flipTransform;
+            }
         }
         private void Button_Click_B(object sender, RoutedEventArgs e)
         {
-            // 90 counter clockwise
-            obrazekBmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            // 90deg counter clockwise (always)
+            if(obrazek != null)
+            {
+                RotateTransform rotateTransform = ImgViewer.LayoutTransform as RotateTransform;
+                if (rotateTransform == null)
+                {
+                    if (FlipX > 0) rotateTransform = new RotateTransform(-90, 0, 0);
+                    else if (FlipX < 0) rotateTransform = new RotateTransform(90, 0, 0);
+                }
+                else
+                {
+                    if (FlipX > 0) rotateTransform.Angle -= 90;
+                    else if (FlipX < 0) rotateTransform.Angle += 90;
+                }
+                ImgViewer.VerticalAlignment = VerticalAlignment.Top;
+                ImgViewer.HorizontalAlignment = HorizontalAlignment.Left;
+                ImgViewer.LayoutTransform = rotateTransform;
+            }
         }
         private void Button_Click_C(object sender, RoutedEventArgs e)
         {
             // only green
             // setPixel
+            if (obrazek != null)
+            {
+
+            }
         }
         private void Button_Click_D(object sender, RoutedEventArgs e)
         {
             // neg
+            if (obrazek != null)
+            {
+
+            }
         }
     }
+
 }
+
