f5bad83277778c659fa531c2cad9eb0f6472d83b
Mikołaj Koszczyc <mikoszczyc@gmail.com>
2021-11-22 19:47:44 +0100

    lab6

---
+++ 22_11_2021/25_10_2021/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+
+namespace _22_11_2021
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<User> items = new List<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            GridList.ItemsSource = items;
+        }
+
+        public class User
+        {
+            public string Name { get; set; }
+            public string Id { get; set; }
+            public int Count { get; set; }
+        }
+
+        public void AddUser(string name, string cnt)
+        {
+            int index = items.Count + 1;
+            items.Add(new User() { Name = name, Id = index.ToString(), Count = Int32.Parse(cnt)});
+            //GridList.ItemsSource = items;
+            GridList.Items.Refresh();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window = new Window1();
+            window.Show();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string saveText = "";
+            foreach(User el in items)
+            {
+                saveText += $"{el.Name},{el.Id},{el.Count}\n";
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, saveText);
+            }
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //clear list
+                items.Clear();
+                foreach (string line in File.ReadLines(openFileDialog.FileName))
+                {
+                    string[] args = line.Split(',');
+                    AddUser(args[0], args[2]);
+                }
+            }
+        }
+    }
+}
---
+++ 22_11_2021/25_10_2021/Window1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace _22_11_2021
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public string nameInput = "";
+        public string countInput = "";
+
+        public Window1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow)Application.Current.MainWindow).AddUser(nameInput, countInput);
+        }
+
+        private void NameInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = e.Source as TextBox;
+            nameInput = textBox.Text;
+            //Debug.WriteLine(nameInput);
+        }
+
+        private void CountInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = e.Source as TextBox;
+            countInput = textBox.Text;
+        }
+    }
+}
