c6fe141967b802b97132e52fd6256794d35b6836
Mikołaj Koszczyc <mikoszczyc@gmail.com>
2021-11-29 21:44:42 +0100

    Finished project 7

--- 29_11_2021/29_11_2021/MainWindow.xaml.cs
+++ 29_11_2021/29_11_2021/MainWindow.xaml.cs
@@ -27,14 +27,70 @@ namespace _29_11_2021
     /// </summary>
     public partial class MainWindow : Window
     {
+        public User selectedUser;
+        public Book selectedBook;
         public List<User> listOfUsers = new List<User>();
         public List<Book> listOfBooks = new List<Book>();
+        public List<User> tmpUsers = new List<User>();
+        public List<Book> tmpBooks = new List<Book>();
 
         public MainWindow()
         {
             InitializeComponent();
             userList.ItemsSource = listOfUsers;
             booksList.ItemsSource = listOfBooks;
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\users_SaveFile.xml") && File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\books_SaveFile.xml"))
+            {
+                LoadFile("users_SaveFile.xml", "books_SaveFile.xml");
+            }
+
+        }
+        void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (listOfUsers.Count() > 0 || listOfBooks.Count() > 0)
+            {
+                AreYouSure popupWindow = new AreYouSure();
+                popupWindow.ShowDialog();
+                e.Cancel = true;
+            }
+        }
+        public void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SerializeToXml(listOfUsers, AppDomain.CurrentDomain.BaseDirectory + "\\users_SaveFile.xml");
+            SerializeToXml(listOfBooks, AppDomain.CurrentDomain.BaseDirectory + "\\books_SaveFile.xml");
+        }
+        void LoadFile(string users_fileName, string books_fileName)
+        {
+            listOfUsers.Clear();
+            listOfBooks.Clear();
+            tmpUsers= DeserializeToObject<List<User>>(AppDomain.CurrentDomain.BaseDirectory + "\\" + users_fileName);
+            tmpBooks = DeserializeToObject<List<Book>>(AppDomain.CurrentDomain.BaseDirectory + "\\" + books_fileName);
+            foreach (User el in tmpUsers)
+            {
+                AddUser(el.user_FirstName, el.user_LastName);
+            }
+            foreach (Book el in tmpBooks)
+            {
+                AddBook(el.book_title, el.book_author,el.book_status);
+            }
+        }
+
+        public static void SerializeToXml<T>(T anyobject, string xmlFilePath)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(anyobject.GetType());
+
+            using (StreamWriter writer = new StreamWriter(xmlFilePath))
+            {
+                xmlSerializer.Serialize(writer, anyobject);
+            }
+        }
+        public T DeserializeToObject<T>(string filepath) where T : class
+        {
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                return (T)serializer.Deserialize(sr);
+            }
         }
         public class User
         {
@@ -63,10 +119,10 @@ namespace _29_11_2021
             listOfUsers.Add(new User() { user_FirstName = firstName, user_LastName = lastName, user_id = index.ToString() });
             userList.Items.Refresh();
         }
-        public void AddBook(string title, string author)
+        public void AddBook(string title, string author, string status = "Nie wypozyczona")
         {
-            int index = listOfUsers.Count + 1;
-            listOfBooks.Add(new Book() {book_title = title,book_author = author, book_id = index.ToString(),book_status = "Nie wypozyczona" });
+            int index = listOfBooks.Count + 1;
+            listOfBooks.Add(new Book() {book_title = title,book_author = author, book_id = index.ToString(),book_status = status });
             booksList.Items.Refresh();
         }
 
@@ -78,15 +134,28 @@ namespace _29_11_2021
 
         private void Borrow_Button_Click(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine(userList.SelectedItem.ToString());
-            
+            if (selectedUser != null && selectedBook != null)
+            {
+                Debug.WriteLine(selectedUser.user_id + " -> " + selectedBook.book_title);
+                if(selectedBook.book_status == "Nie wypozyczona")
+                {
+                    (booksList.SelectedItem as Book).book_status = selectedUser.user_id;
+                    booksList.Items.Refresh();
+                }
+            }
         }
-
         private void Return_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            if (selectedUser != null && selectedBook != null)
+            {
+                Debug.WriteLine(selectedUser.user_id + " -> " + selectedBook.book_title);
+                if (selectedBook.book_status == selectedUser.user_id)
+                {
+                    (booksList.SelectedItem as Book).book_status = "Nie wypozyczona";
+                    booksList.Items.Refresh();
+                }
+            }
         }
-
         private void AddBook_Button_Click(object sender, RoutedEventArgs e)
         {
             AddBook addBook_window = new AddBook();
@@ -95,12 +164,12 @@ namespace _29_11_2021
 
         private void userList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            User selectedUser = userList.SelectedItems[0] as User;
+            selectedUser = userList.SelectedItems[0] as User;
         }
 
         private void booksList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Book selectedBook = booksList.SelectedItems[0] as Book;
+            selectedBook = booksList.SelectedItems[0] as Book;
         }
     }
 }
