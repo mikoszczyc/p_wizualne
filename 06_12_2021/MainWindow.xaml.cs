201115fcec99bb8b2f7ff0c10da59bf9b5a9c1b7
mikoszczyc <mikoszczyc@gmail.com>
2021-12-06 16:11:47 +0100

    beginning

---
+++ 06_12_2021/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Xml.Serialization;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace _06_12_2021
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public class Airport
+    {
+        [XmlAttribute("airport_name")]
+        public string airport_name { get; set; }
+    }
+        public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
