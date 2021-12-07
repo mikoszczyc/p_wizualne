ad2c56cc67bc7249ad731bb6335f3d7f49661470
Mikołaj Koszczyc <mikoszczyc@gmail.com>
2021-12-07 01:09:54 +0100

    Finished lab8

--- 06_12_2021/MainWindow.xaml.cs
+++ 06_12_2021/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using System.Configuration;
 using System.Collections.Specialized;
 using System.Reflection;
 using System.Diagnostics;
+using Microsoft.VisualBasic.FileIO;
 
 namespace _06_12_2021
 {
@@ -29,14 +30,89 @@ namespace _06_12_2021
 
     public class Airport
     {
-        [XmlAttribute("airport_name")]
-        public string airport_name { get; set; }
+        [XmlAttribute("mainCity")]
+        public string mainCity { get; set; }
+        [XmlAttribute("voivodeship")]
+        public string voivodeship { get; set; }
+        [XmlAttribute("ICAO")]
+        public string ICAO { get; set; }
+        [XmlAttribute("IATA")]
+        public string IATA { get; set; }
+        [XmlAttribute("airportName")]
+        public string airportName { get; set; }
+        [XmlAttribute("numberOfPassengers")]
+        public string numberOfPassengers { get; set; }
+        [XmlAttribute("change")]
+        public string change { get; set; }
+
+        public Airport(string mainCity, string voivodeship, string ICAO, string IATA, string airportName, string numberOfPassengers, string change)
+        {
+            this.mainCity = mainCity;
+            this.voivodeship = voivodeship;
+            this.ICAO = ICAO;
+            this.IATA = IATA;
+            this.airportName = airportName;
+            this.numberOfPassengers = numberOfPassengers;
+            this.change = change;
+        }
     }
         public partial class MainWindow : Window
     {
+        public Airport selectedAirport;
+        public List<Airport> Airports = new List<Airport>();
+        public string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
         public MainWindow()
         {
             InitializeComponent();
+            if (File.Exists(path + "\\Test_Data.csv"))
+            {
+                LoadFile("Test_Data.csv");
+                airportsList.ItemsSource = Airports;
+                airportsList.Items.Refresh();
+            }
+            else
+                Console.WriteLine("Sorry, no input file found!");
+            
+        }
+
+        void LoadFile(string fileName)
+        {
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            //string fileContent = File.ReadAllText(path + "\\" + fileName, Encoding.UTF8);
+            
+            using (TextFieldParser csvParser = new TextFieldParser(path + "\\" + fileName))
+            {
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                csvParser.ReadLine();
+                csvParser.ReadLine();
+
+
+                while (!csvParser.EndOfData)
+                {
+                    string[] fields = csvParser.ReadFields();
+                    Airport newAirport = new Airport(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6]);
+                    Airports.Add(newAirport);
+                }
+            }
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Summary window = new Summary();
+
+            if (selectedAirport != null)
+            {
+                window.Show();
+            }
+        }
+
+        private void airportsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedAirport = airportsList.SelectedItems[0] as Airport;
+            Debug.WriteLine(selectedAirport.airportName);
         }
     }
 }
---
+++ 06_12_2021/Summary.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace _06_12_2021
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Summary.xaml
+    /// </summary>
+    public partial class Summary : Window
+    {
+        public Summary()
+        {
+            InitializeComponent();
+            var origin = ((MainWindow)Application.Current.MainWindow);
+            if (origin.checkbox_ICAO.IsChecked ?? true)
+            {
+                Debug.WriteLine("ICAO = TRUE");
+
+                label1.Content = "Kod ICAO: " + origin.selectedAirport.ICAO;
+            }
+            if (origin.checkbox_IATA.IsChecked ?? true)
+            {
+                Debug.WriteLine("IATA = TRUE");
+
+                label2.Content = "Kod IATA: " + origin.selectedAirport.IATA;
+
+            }
+            if (origin.checkbox_Passengers.IsChecked ?? true)
+            {
+                Debug.WriteLine("Passengers = TRUE");
+
+                label3.Content = "Liczba pasażerów: " + origin.selectedAirport.numberOfPassengers;
+            }
+            if (origin.checkbox_Voivodeship.IsChecked ?? true)
+            {
+                Debug.WriteLine("Voivodeship = TRUE");
+
+                label4.Content = "Województwo: " + origin.selectedAirport.voivodeship;
+            }
+            if (origin.checkbox_City.IsChecked ?? true)
+            {
+                Debug.WriteLine("City = TRUE");
+
+                label5.Content = "Miasto: " + origin.selectedAirport.mainCity;
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
