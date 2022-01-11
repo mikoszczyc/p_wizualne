a027c4c25087a77501d3cb1c2cef8fa3f7a9b35e
Mikołaj Koszczyc <mikoszczyc@gmail.com>
2022-01-11 13:27:32 +0100

    Working grid generation

--- 10_01_2022/GameBoard.xaml.cs
+++ 10_01_2022/GameBoard.xaml.cs
@@ -21,6 +21,9 @@ namespace _10_01_2022
     {
         public GameBoard(string animal, string difficulty)
         {
+            InitializeComponent();
+            int boardSize = 3;
+            
             switch (animal)
             {
                 case "Mouse":
@@ -35,16 +38,62 @@ namespace _10_01_2022
             switch (difficulty)
             {
                 case "Easy":
+                    boardSize = 3;
                     break;
                 case "Normal":
+                    boardSize = 4;
                     break;
                 case "Hard":
+                    boardSize = 5;
                     break;
                 case "Hard++":
+                    boardSize = 5;
                     break;
             }
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    Rectangle myRect = new Rectangle();
+                    switch (difficulty)
+                    {
+                        case "Easy":
+                            myRect.Stroke = Brushes.ForestGreen;
+                            myRect.Fill = Brushes.SpringGreen;
+                            break;
+                        case "Normal":
+                            myRect.Stroke = Brushes.DeepSkyBlue;
+                            myRect.Fill = Brushes.BlueViolet;
+                            break;
+                        case "Hard":
+                            myRect.Stroke = Brushes.DarkRed;
+                            myRect.Fill = Brushes.MediumPurple;
+                            break;
+                        case "Hard++":
+                            myRect.Stroke = Brushes.LightGoldenrodYellow;
+                            myRect.Fill = Brushes.MediumVioletRed;
+                            break;
+                    }
+                    myRect.HorizontalAlignment = HorizontalAlignment.Center;
+                    myRect.VerticalAlignment = VerticalAlignment.Center;
+                    myRect.Height = 90;
+                    myRect.Width = 90;
+                    myRect.MouseLeftButtonDown += hideRect;
 
-            InitializeComponent();
+                    GameGrid.Children.Add(myRect);
+                    Grid.SetRow(myRect, i);
+                    Grid.SetColumn(myRect, j);
+                }
+            }
+
+        }
+        private void hideRect(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle clickedRect = e.Source as Rectangle;
+            if (clickedRect.IsVisible)
+            {
+                clickedRect.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
